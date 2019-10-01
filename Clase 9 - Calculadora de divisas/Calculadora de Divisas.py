987c0ab5d1c4e95d92da8671356816e61c6d6d9c
Juan Diego <dippons78@gmail.com>
2019-10-01 13:42:22 -0500

    Calculadora corregida

--- Clase 9 - Calculadora de divisas/Calculadora de Divisas.py
+++ Clase 9 - Calculadora de divisas/Calculadora de Divisas.py
@@ -1,20 +1,23 @@
+#Método de conversión de pesos mexicanos a colombianos
 def foreign_exchange_calculator(ammount):
     mex_to_col_rate = 145.97
     return mex_to_col_rate * ammount
 
+#Método dónde comienza el programa
 def run():
     print('C A L C U L A D O R A  D E  D I V I S A S')
     print('Convierte pesos mexicanos a pesos colombianos')
     print('')
-
+    #Captura la cantidad de pesos mexicanos
     ammount = float(input('Ingrese la cantidad de pesos ' +
      'mexicanos que desee convertir'))
-
+    #Resultado de ejecutar el metodo de convertir pesos mexicanos a colombianos
+    #Se pueden asignar variables a métodos
     result = foreign_exchange_calculator(ammount)
 
-    print('${}pesos mexicanos son ${} pesos colombianos'.format(ammount,result))
+    print('${} pesos mexicanos son ${} pesos colombianos'.format(ammount,result))
     print('')
 
-
+#Indica desde dónde comienza a ejecutar Python el programa
 if __name__ == '__main__':
     run()
