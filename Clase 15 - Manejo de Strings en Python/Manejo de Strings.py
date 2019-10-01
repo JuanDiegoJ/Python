8b1b807912ae7949191fce4ae85d1f04c02fca22
Juan Diego <dippons78@gmail.com>
2019-10-01 17:03:18 -0500

    Actualizar clases 14 y 15

---
+++ Clase 15 - Manejo de Strings en Python/Manejo de Strings.py
@@ -0,0 +1,14 @@
+cadena = 'Juan Diego y Fernanda :3'
+
+print('Comando len: ' + str(len(cadena)))
+print('Comando capitalize: ' + cadena.capitalize())
+print('Comando upper: ' + cadena.upper())
+print('Comando isupper: ' + str(cadena.isupper()))
+print('Comando lower: ' + str(cadena.lower))
+print('Comando islower: ' + str(cadena.islower()))
+print('Comando find: ' + str(cadena.find('F')))
+print('Comando isdigit: ' + str(cadena.isdigit('a')))
+print('Comando endswith: ' + str(cadena.endswith('3')))
+print('Comando starswith: ' + cadena.startswith('J'))
+print('Comando split: ' + cadena.split)
+print('Comando join: ' + cadena.join(['asd'],['ba']))
