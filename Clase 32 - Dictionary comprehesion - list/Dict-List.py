f93e1aae75160dcb7a192617362a7efe0b64cba0
Juan Diego <dippons78@gmail.com>
2019-10-04 09:04:08 -0500

    Actualización Clase 30

---
+++ Clase 32 - Dictionary comprehesion - list/Dict-List.py
@@ -0,0 +1,15 @@
+#List-comprehension
+pares = []
+for num in range (1, 31):
+    if num % 2 == 0:
+        pares.append(num)
+print(pares)
+even = [num for num in range(1,31) if num % 2 == 0]
+print(even)
+#Dictionary-Comprehesion
+cuadrados = {}
+for num in range(1,11):
+    cuadrados[num] = num**2
+print(cuadrados)
+squares = {num: num**2 for num in range(1, 11)}
+print(squares)
