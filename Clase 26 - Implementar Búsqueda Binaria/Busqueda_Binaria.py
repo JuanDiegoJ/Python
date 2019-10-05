d5df1b9381292f15af10b01a31368d8d0b26ecdc
Juan Diego <dippons78@gmail.com>
2019-10-05 09:03:06 -0500

    Actualizar

--- Clase 26 - Implementar Búsqueda Binaria/Busqueda_Binaria.py
+++ Clase 26 - Implementar Búsqueda Binaria/Busqueda_Binaria.py
@@ -8,7 +8,7 @@ def binary_search(numbers, number_to_find, low, high):
     elif numbers[mid] > number_to_find:
         return binary_search(numbers, number_to_find, low, mid-1)
     else:
-        return binary_search(number12s, number_to_find, mid-1, high)
+        return binary_search(numbers, number_to_find, mid+1, high)
 
 if __name__ == '__main__':
     numbers = [1, 3, 4, 6, 7, 8, 10, 12, 16, 18, 25, 26, 30]
